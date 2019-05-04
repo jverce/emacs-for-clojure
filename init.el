466535450697ef032789793daec8723cfd9693ea
jverce <juanjov@gmail.com>
2019-05-04 01:45:35 -0700

    Fix CIDER issue when starting session

--- init.el
+++ init.el
@@ -145,10 +145,11 @@
  '(coffee-tab-width 2)
  '(package-selected-packages
    (quote
-    (magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell))))
+    (cider-decompile elein magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(defun sesman-linked-sessions (x y))
